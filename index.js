7bfb75ccd6b48183b47d2295af36196841e8a4ea
MarcoMadera <marcoseguro2@gmail.com>
2020-06-28 01:38:25 -0500

    making queries

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 "use strict";
-const { graphql, buildSchema } = require("graphql");
+const { buildSchema } = require("graphql");
 const express = require("express");
 const gqlMiddleware = require("express-graphql");
+const { readFileSync } = require("fs");
+const { join } = require("path");
+const resolvers = require("./lib/resovers");
 
 const app = express();
 const port = process.env.port || 3000;
-//Schema
-const schema = buildSchema(`
-type Query {
-  name: String
-  message: String
-}`);
-
-//Resolvers
-const resolvers = {
-  name: () => {
-    return "Marco";
-  },
-  message: () => {
-    return "The beggining";
-  },
-};
+// Schema
+const schema = buildSchema(
+  readFileSync(join(__dirname, "lib", "schema.graphql"), "utf-8")
+);
 
 app.use(
   "/api",
@@ -35,7 +26,7 @@ app.listen(port, () => {
   console.log(`The project is running at http://localhost:${port}/api`);
 });
 
-//exe
+// exe
 // graphql(schema, "{ message }", resolvers).then((data) => {
 //   console.log(data);
 // });
